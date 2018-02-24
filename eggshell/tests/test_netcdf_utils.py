05f27b89b599444528f2678886705c7a2cda50c9
nilshempelmann <info@nilshempelmann.de>
2018-02-24 18:27:24 +0100

    sphinx config

--- eggshell/tests/test_netcdf_utils.py
+++ eggshell/tests/test_netcdf_utils.py
@@ -1,5 +1,12 @@
 def test_libs():
-    from ocgis import OcgOperations, RequestDataset
-    from ocgis import env, DimensionMap, crs
-    from ocgis.util.large_array import compute
-    from ocgis.util.helpers import get_sorted_uris_by_time_dimension
+    from netCDF4 import Dataset, MFDataset
+    from netCDF4 import num2date
+
+def test_iris():
+    from iris.analysis import cartography as ct
+    import iris.cube
+    import iris.analysis
+    from iris.analysis._area_weighted import AreaWeightedRegridder
+    from iris.analysis._interpolation import get_xy_dim_coords, snapshot_grid
+    from iris.analysis.cartography import wrap_lons as wrap_circular_points
+    import cf_units
