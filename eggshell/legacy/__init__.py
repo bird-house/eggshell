96cc9607cc164ea4945020bc7da96ede404cfe3e
Pingu Carsti <ehbrecht@dkrz.de>
2018-11-19 14:23:42 +0100

    update legacy docu

--- eggshell/legacy/__init__.py
+++ eggshell/legacy/__init__.py
@@ -1,1 +1,6 @@
-__version__ = 0.3
+"""
+The legacy package contains code copied from `FlyingPigeon`.
+
+.. warning:: The legacy package is deprecated and needs to be refactored for common usage.
+
+"""
