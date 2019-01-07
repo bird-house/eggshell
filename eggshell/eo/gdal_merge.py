184385311f7889d33ce95ec6edb354793c80a6bf
nilshempelmann <info@nilshempelmann.de>
2019-01-07 23:57:21 +0100

    pep8

--- eggshell/eo/gdal_merge.py
+++ eggshell/eo/gdal_merge.py
@@ -64,9 +64,8 @@ def GetOutputDriversFor(filename):
     ext = GetExtension(filename)
     for i in range(gdal.GetDriverCount()):
         drv = gdal.GetDriver(i)
-        if (drv.GetMetadataItem(gdal.DCAP_CREATE) is not None or
-            drv.GetMetadataItem(gdal.DCAP_CREATECOPY) is not None) and
-        drv.GetMetadataItem(gdal.DCAP_RASTER) is not None:
+        if (drv.GetMetadataItem(gdal.DCAP_CREATE) is not None or drv.GetMetadataItem(
+                gdal.DCAP_CREATECOPY) is not None) and drv.GetMetadataItem(gdal.DCAP_RASTER) is not None:
             if len(ext) > 0 and DoesDriverHandleExtension(drv, ext):
                 drv_list.append(drv.ShortName)
             else:
